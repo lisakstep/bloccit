05f40769db4c8065ea1e010e3e4e293841471b1f
lisakstep <lisakstep@gmail.com>
2014-11-03 04:38:18 +0000

    Associated and nested topics plus some refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,21 @@ require 'faker'
 end
 users = User.all
 
+# Create topics
+15.times do
+  Topic.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
+
+
 # Create posts
 50.times do 
   Post.create!(
     user: users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -31,10 +42,32 @@ posts = Post.all
     )
 end
 
-User.first.update_attributes!(
-  email: 'lisakstep@gmail.com',
-  password: 'lookatme'
+admin = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
   )
+admin.skip_confirmation!
+admin.save!
+
+moderator = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com',
+  password: 'helloworld',
+  role: 'moderator'
+  )
+moderator.skip_confirmation!
+moderator.save!
+
+member = User.new(
+  name: 'Member User',
+  email: 'member@example.com',
+  password: 'helloworld',
+  role: 'member'
+  )
+member.skip_confirmation!
+member.save!
 
 puts "Seeding finished"
 puts "#{User.count} users created"
