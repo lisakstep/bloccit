fcf501e3980d7d9886aad05867cf4ec86b75dc33
lisakstep <lisakstep@gmail.com>
2014-11-11 23:04:31 +0000

    improved nested routing clarity

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Bloccit::Application.routes.draw do
  
   get "comments/new"
   devise_for :users
-    resources :users, only: [:update]
+  resources :users, only: [:update]
 
   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-    end
+    resources :posts, except: [:index] 
   end
 
-  resources :posts do
+  resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
   end
   
