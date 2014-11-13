f59aa15768efb68b538ef9029cdea67f0a415df2
lisakstep <lisakstep@gmail.com>
2014-11-13 20:43:18 +0000

    new posts start off with user's upvote

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
+  after_create :create_vote
+
   def up_votes
     votes.where(value: 1).count
   end
@@ -31,4 +33,12 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   #validates :topic, presence: true
   #validates :user, presence: true
+
+  private
+
+  def create_vote
+    #user.votes.create
+    user.votes.create(value: 1, post: self)
+
+  end
 end
