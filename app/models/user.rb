bebe57ebeccf0f68e3a30064fbf021412623dfd7
lisakstep <lisakstep@gmail.com>
2014-11-17 21:53:34 +0000

    misc fixes after conference with Xander

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,10 +21,10 @@ class User < ActiveRecord::Base
   end
 
   def favorited(post)
-    favorites.where(post_id: post.id).first
+    favorites.find_by_post_id post.id
   end
   
   def voted(post)
-    votes.where(post_id: post.id).first
+    votes.find_by_post_id post.id
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,24 @@
 require 'rails_helper'
 
+include TestFactories
+include Devise::TestHelpers
+
 describe User do 
 
-  include TestFactories
-  include Devise::TestHelpers
+  
 
   describe '#favorited(post)' do
 
     before do
-      @user = authenticated_user
       @post = associated_post
-      sign_in @user
+      @user = authenticated_user
     end
 
-    xit "returns nil if the user has not favorited the post" do
+    it "returns nil if the user has not favorited the post" do
       expect( @user.favorited(@post) ).to be_nil
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns the appropriate favorite if it exists" do
       favorite = @user.favorites.where(post: @post).create
       expect( @user.favorited(@post) ).to eq(favorite)
     end
