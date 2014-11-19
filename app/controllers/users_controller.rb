731217fe2cd42972ad99894fc941e48dafb06f02
lisakstep <lisakstep@gmail.com>
2014-11-19 21:21:25 +0000

    Added 'Active Posts' successfully. 'Active Users' not working correctly. @users collection is nil. Also implemented query perfomance improvements successfully.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :avatar, :email_favorites)
   end
 
+  def index
+    @users = User.top_rated #.paginate(page: params[:page], per_page: 10)
+  end
 end
